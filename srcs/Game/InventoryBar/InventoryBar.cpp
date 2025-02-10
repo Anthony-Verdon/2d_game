d91990d06cafec85273f161aefed7c40cbe3c9a4
Anthony Verdon <anthonyverdon10@gmail.com>
2025-02-10 01:59:04 +0100

    add a way to draw inventory slots

--- srcs/Game/InventoryBar/InventoryBar.cpp
+++ srcs/Game/InventoryBar/InventoryBar.cpp
@@ -19,11 +19,12 @@ void InventoryBar::Draw(const Player &player)
     glm::vec2 windowSize = WindowManager::GetWindowSize() / 2.0f;
     glm::vec2 topLeftCorner = -windowSize + playerPos;
 
-    DrawInventorySlot(topLeftCorner, glm::vec2(5, 3));
-    
+    glm::vec2 backgroundSize = glm::vec2(5, 5);
+    DrawInventorySlotBackground(topLeftCorner, backgroundSize);
+    DrawMultipleSlots(topLeftCorner, backgroundSize, glm::vec2(2, 2));
 }
 
-void InventoryBar::DrawInventorySlot(const glm::vec2 &position, const glm::vec2 &size)
+void InventoryBar::DrawInventorySlotBackground(const glm::vec2 &position, const glm::vec2 &size)
 {
     auto texture = RessourceManager::GetTexture("UI_Frames");
     size_t width = texture->getWidth();
@@ -38,6 +39,43 @@ void InventoryBar::DrawInventorySlot(const glm::vec2 &position, const glm::vec2 
         {
             sprite.spriteCoords.x = DetermineSpriteCoord(x, size.x - 1);
             sprite.spriteCoords.y = DetermineSpriteCoord(y, size.y - 1);
+            SpriteRenderer::Draw(position + glm::vec2(x, y) * SLOT_SIZE, glm::vec2(SLOT_SIZE, SLOT_SIZE), 0, glm::vec3(1, 1, 1), sprite, false, false, 1);
+        }
+    }
+}
+
+void InventoryBar::DrawMultipleSlots(const glm::vec2 &position, const glm::vec2 &backgroundSize, const glm::vec2 &nbSlot)
+{
+    int corner = 2;
+    glm::vec2 nbGap;
+    nbGap.x = corner + nbSlot.x - 1;
+    nbGap.y = corner + nbSlot.y - 1;
+    glm::vec2 gapSize;
+    gapSize.x = ((backgroundSize.x - corner) * SLOT_SIZE - nbSlot.x * SLOT_SIZE) / nbGap.x;
+    gapSize.y = ((backgroundSize.y - corner) * SLOT_SIZE - nbSlot.y * SLOT_SIZE) / nbGap.y;
+    for (int x = 0; x < nbSlot.x; x++)
+    {
+        for (int y = 0; y < nbSlot.y; y++)
+        {
+            DrawInventorySlot(position + glm::vec2(x, y) * SLOT_SIZE + glm::vec2(x + 1, y + 1) * gapSize);
+        }
+    }
+}
+
+void InventoryBar::DrawInventorySlot(const glm::vec2 &position)
+{
+    auto texture = RessourceManager::GetTexture("UI_Frames");
+    size_t width = texture->getWidth();
+    size_t height = texture->getHeight();
+    Sprite sprite;
+    sprite.textureName = "UI_Frames";
+    sprite.textureSize = glm::vec2(width, height) /  TILE_SIZE;
+
+    for (int x = 0; x < 3; x++)
+    {
+        for (int y = 0; y < 3; y++)
+        {
+            sprite.spriteCoords = glm::vec2(3, 0) + glm::vec2(x, y);
             SpriteRenderer::Draw(position + glm::vec2(x, y) * SLOT_SIZE, glm::vec2(SLOT_SIZE, SLOT_SIZE), 0, glm::vec3(1, 1, 1), sprite, false, false, 1);
         }
     }
--- srcs/Game/InventoryBar/InventoryBar.hpp
+++ srcs/Game/InventoryBar/InventoryBar.hpp
@@ -5,7 +5,9 @@
 class InventoryBar
 {
     private:
-        void DrawInventorySlot(const glm::vec2 &position, const glm::vec2 &size);
+        void DrawInventorySlotBackground(const glm::vec2 &position, const glm::vec2 &size);
+        void DrawMultipleSlots(const glm::vec2 &position, const glm::vec2 &backgroundSize, const glm::vec2 &nbSlot);
+        void DrawInventorySlot(const glm::vec2 &position);
         int DetermineSpriteCoord(int coord, int size);
 
     public:
