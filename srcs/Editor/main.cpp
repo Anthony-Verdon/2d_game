e056351498ddedc9e79fbd4a438be2a2be828693
Anthony Verdon <anthonyverdon10@gmail.com>
2026-05-15 15:23:07 +0200

    fix transparency of Sprite rendering

--- srcs/Editor/main.cpp
+++ srcs/Editor/main.cpp
@@ -7,9 +7,11 @@
 int main()
 {
     // @todo: put ImGUI as a parameter in WindowManager to destroy it correctly
-    // actually, it's destroye in ~Editor which cause errors for GLFW
+    // actually, it's destroyed in ~Editor which cause errors for GLFW
     std::cout << "welcome in the editor" << std::endl;
     WindowManager::InitWindow("new editor");
+    WindowManager::SetClearColor(0.5, 0.5, 0.5, 0.5);
+    WindowManager::EnableBlend(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     Editor editor;
     WindowManager::StartUpdateLoop(&editor);
     WindowManager::DestructWindowManager();
--- srcs/Game/main.cpp
+++ srcs/Game/main.cpp
@@ -7,6 +7,8 @@ int main()
 {
     std::cout << "welcome in the new game" << std::endl;
     WindowManager::InitWindow("new game");
+    WindowManager::SetClearColor(0.5, 0.5, 0.5, 0.5);
+    WindowManager::EnableBlend(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     Game game;
     WindowManager::StartUpdateLoop(&game);
     WindowManager::DestructWindowManager();
